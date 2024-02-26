76184c389780af821b6032cd8b31a513a3b84d77
mario <codemario318@gmail.com>
2024-02-26 20:14:30 +0900

    [UPDATE] Controller method mapping 수정

--- src/main/kotlin/com/mario/webflux/controller/AppUserController.kt
+++ src/main/kotlin/com/mario/webflux/controller/AppUserController.kt
@@ -4,23 +4,30 @@ import com.mario.webflux.model.AppUser
 import com.mario.webflux.service.AppUserService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 class AppUserController(
     private val appUserService: AppUserService
 ) {
-    @GetMapping("/user")
+    @GetMapping
     fun getAll(): Flux<AppUser> {
         return appUserService.findAll()
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     fun getById(@PathVariable id: Long): Mono<AppUser> {
         return appUserService.findById(id)
+    }
+
+    @PostMapping
+    fun createUser(@RequestBody appUserRequest: AppUserRequest): Mono<AppUser> {
+        return appUserService.createUser(appUserRequest)
     }
 }
